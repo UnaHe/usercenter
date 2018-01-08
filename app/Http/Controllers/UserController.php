a3c5fc3270e43e4672fff2990340e232d2b9c8c3
formatcc <young123tao@163.com>
2018-01-08 10:00:28 +0800

    '认证通过返回用户信息'

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     public function checkAuth(){
         $user= Auth::guard("api")->user();
         if($user){
-            return $this->ajaxSuccess(['id'=>$user->id]);
+            return $this->ajaxSuccess($user);
         }else{
             return $this->ajaxError();
         }
